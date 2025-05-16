1865d108209f8c961cb2443bb826b947ce4df419
Jose A. Ruiz <jruiz261@toromail.csudh.edu>
2025-05-15 21:35:55 -0700

    Update root message to celebrate first domain

--- src/controllers/root/getRoot.js
+++ src/controllers/root/getRoot.js
@@ -6,7 +6,29 @@ import config from '../../config.js';
  * @param {import('express').Response} res 
  */
 const getRoot = (_req, res) => {
-  res.status(200).send('🚀 I just obtained my first domain and deployed my Express app! 😎🎉');
+  res.status(200).send(`
+      <html>
+          <head>
+              <title>My First Domain</title>
+              <style>
+                  body {
+                      background-color: #282c34;
+                      color: #61dafb;
+                      font-family: Arial, sans-serif;
+                      text-align: center;
+                      padding-top: 100px;
+                  }
+                  h1 {
+                      font-size: 3rem;
+                  }
+              </style>
+          </head>
+          <body>
+              <h1>🚀 I just obtained my first domain!</h1>
+              <p>And deployed my Express app with style 🎉</p>
+          </body>
+      </html>
+  `);
 }
 
 export default getRoot;
